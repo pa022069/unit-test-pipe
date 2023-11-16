65a7c7603c46fd7811ca6e4f89e6929933bad3e3
pa022069 <pa022069@gmail.com>
2023-11-16 18:13:05 +0800

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
 export default function Page() {
+  const temp1 = () => {
+    return {
+      var1: 1,
+    };
+  };
+  const temp2 = () => {
+    return {
+      var2: 2,
+    };
+  };
+  const temp3 = () => {
+    return {
+      var3: 3,
+    };
+  };
+  const func = (_array: any[]) => {
+    const obj = {};
+    _array.forEach(item => {
+      Object.keys(item).forEach(key => {
+        obj[key] = item[key];
+      });
+    });
+    return obj;
+  };
+
+  console.log('func', func([temp1(), temp2(), temp3()]));
+
   return <div>Index Page</div>;
 }
